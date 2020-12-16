d7e8d1a826ad89ff9dbb9e7c652727b257f76f2b
Héctor González Pereda <hgonza02@ucm.es>
2020-12-16 20:25:08 +0100

    add conection, add index

--- Index.php
+++ Index.php
@@ -1,6 +1,25 @@
  <?php
     include ("connection.php");
-    $con=conectar();
    echo "Se ha realizado exitosamente la conexion";
    
+   $query = $con->prepare("SELECT * FROM actor");
+   
+   $query->execute();
+
  ?>
+
+
+<?php 
+  
+  while($row = $query->fetch()){
+
+    echo $row ["actor_id"] . '<br>';
+    echo $row ["first_name"] . '<br>';
+    echo $row ["last_name"] . '<br>';
+    
+
+
+
+  }
+
+?>
--- connection.php
+++ connection.php
@@ -1,12 +1,11 @@
 <?php
-function conectar(){
+
     $user="root";
     $pass="";
     $server="localhost";
-    $db="m07";
-    $con= mysqli_connect($server, $user, $pass) or die ("Ha saltado un error al conectar" .mysqli_connect());
+    $db= "sakila";
+      $con = new PDO("mysql:host=localhost;dbname=sakila", "root", "" ) or die ("Ha saltado un error al conectar" .mysqli_connect());
     
-    return $con;
+  ?> 
     
-}
-?>
+
